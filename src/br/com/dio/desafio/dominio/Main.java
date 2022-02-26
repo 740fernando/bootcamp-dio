fe1b2b945dda2682d3691dabca99a115e06a185c
Fernando <fernando.luiz92@hotmail.com>
2022-02-26 13:41:59 -0300

    Abstraindo um Bootcamp Usando Orientação a Objetos em Java

---
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -0,0 +1,26 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Main {
+	public static void main(String[] args) {
+		Curso curso = new Curso();
+		curso.setTitulo("Bootcamp JAVA");
+		curso.setDescricao("\r\n" + "Abstraindo um Bootcamp Usando Orientação a Objetos em Java");
+		curso.setCargaHoraria(8);
+
+		Curso outroCurso = new Curso();
+		outroCurso.setTitulo("Bootcamp c#");
+		outroCurso.setDescricao("\r\n" + "Abstraindo um Bootcamp Usando Orientação a Objetos em c sharp");
+		outroCurso.setCargaHoraria(8);
+
+		Mentoria mentoria = new Mentoria();
+		mentoria.setTitulo("mentoria bootcamp java");
+		mentoria.setDescricao("descrição mentoria java");
+		mentoria.setData(LocalDate.now());
+
+		System.out.println(curso);
+		System.out.println(outroCurso);
+		System.out.println(mentoria);
+	}
+}
